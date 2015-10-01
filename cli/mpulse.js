232256446df761c99f33705fc790a9db5afa3318
Andreas Marschke <andreas.marschke@gmail.com>
2015-09-30 17:37:35 -0700

    Use Subclassing introduced in repository.js

--- cli/mpulse.js
+++ cli/mpulse.js
@@ -6,5 +6,32 @@ var fs = require("fs");
 
 module.exports = function(type, params, options) {
     cmdCore.init(options);
-    log.debug(params);
+    var properties;
+
+    if (!options.parent.apikey) {
+        log.error("--apikey (-a) is required!");
+        process.exit(1);
+    }
+
+    try {
+        properties = JSON.parse(params);
+    }
+    catch(exception) {
+        log.error("Parsing parameters failed!");
+        process.exit(1);
+    }
+
+    log.debug("Querying  for Type: " + type + " with params: " + params);
+
+    cmdCore.connectToRepository(options, function(err, repo) {
+        cmdCore.handleError(err);
+        repo.setApiKey(options.parent.apikey);
+
+        repo.mPulse(type, properties, function(err, result) {
+
+            cmdCore.handleError(err);
+            log.info(result);
+
+        });
+    });
 };
